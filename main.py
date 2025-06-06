04e236f0d53a950ad55c5da6c7f7e9a2e9e03a5b
Cutey-Whity1 <o45043257@gmail.com>
2025-06-07 01:13:57 +0300

    fixed?

--- main.py
+++ main.py
@@ -3,8 +3,3 @@ import config
 
 print(config.token)
 bot = telebot.TeleBot(config.token)
-
-@bot.message_handler(content_types=['new_chat_members'])
-def make_some(message):
-    bot.send_message(message.chat.id, 'I accepted a new user!')
-    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
